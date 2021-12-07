3f48fc04b323292fc6a7d4b6371b0364a2352cb3
Daniel Denton <danielryandenton@gmail.com>
2021-12-07 11:17:01 -0600

    refactor using static method

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('sequelize');
+const { Post, User, Vote } = require('../../models');
 
 // GET all posts
 router.get('/', (req, res) => {
   console.log('==='.repeat(5));
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [
+        sequelize.literal(
+          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+        ),
+        'vote_count',
+      ],
+    ],
     order: [['created_at', 'DESC']],
     include: [
       {
@@ -29,7 +41,18 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [
+        sequelize.literal(
+          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+        ),
+        'vote_count',
+      ],
+    ],
     include: [
       {
         model: User,
@@ -62,6 +85,17 @@ router.post('/', (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
+    });
+});
+
+// UPDATE post with upvote
+router.put('/upvote', (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
 });
 
